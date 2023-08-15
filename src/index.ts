c7443e77f6b4c70c61db8b7c78b129faec230297
heclair <heclair@hotmail.com>
2023-08-15 19:53:44 -0300

    aula 02

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("teste")
+class Pessoa {
+  nome: string; // nome e idade são variáveis
+  idade: number;
+
+constructor(a:string,b:number){
+    this.nome = a;
+    this.idade = b;
+}
+
+  imprimir() {
+    //nome seguido de abre e fecha parenteses é função, dentro da classe é um método
+    console.log(this.nome, this.idade);
+  }
+}
+
+const p = new Pessoa("Heclair",28);
+const q = new Pessoa ("Marcelo",22);
+
+p.imprimir();
+q.imprimir();
